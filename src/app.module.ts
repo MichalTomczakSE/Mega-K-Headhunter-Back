56f5c10bf01da832fdb33bd6b9261c8df4cb43bc
MichalTomczakSE <116025688+MichalTomczakSE@users.noreply.github.com>
2023-04-20 19:03:00 +0200

    Slightly code refactor - change imports structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ormConfig } from '../ormconfig';
 
 @Module({
