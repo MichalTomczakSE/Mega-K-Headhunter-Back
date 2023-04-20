3421c1241abbb9ee17ed808980183ea271b138f9
MichalTomczakSE <116025688+MichalTomczakSE@users.noreply.github.com>
2023-04-20 19:00:29 +0200

    Change port value to env variable.

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,7 +4,7 @@ const serverCa = [readFileSync("./DigiCertGlobalRootCA.crt.pem", "utf8")];
 export const ormConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host:  process.env.MYSQL_HOST,
-  port: 3306,
+  port: process.env.MYSQL_PORT,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
