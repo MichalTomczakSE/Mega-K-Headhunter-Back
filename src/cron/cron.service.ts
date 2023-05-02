087bbe1393e74886a201baa656d6c3b82df9b0c4
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-05-02 18:29:43 +0200

    Added cron clearing awaiting students above 10 days

---
+++ src/cron/cron.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { LessThan } from 'typeorm';
+import * as moment from 'moment';
+import { Student } from '../student/student.entity';
+import { StudentStatus } from '../types';
+
+@Injectable()
+export class CronService {
+    @Cron(CronExpression.EVERY_DAY_AT_1AM)
+    async ClearUnhiredStudents(): Promise<void> {
+        const lastValidDate = new Date(
+            moment().subtract(10, 'd').format('YYYY-MM-DD'),
+        );
+
+        const unhiredStudents = await Student.find({
+            select: ['id'],
+            where: {
+                status: StudentStatus.awaiting,
+                isActive: true,
+                scheduledAt: LessThan(lastValidDate),
+            },
+        });
+        const unhiredStudentsIdList = unhiredStudents.map((student) => student.id);
+        console.log(unhiredStudentsIdList);
+
+        if ( unhiredStudents.length < 1 ) return;
+
+        await Student.update(unhiredStudentsIdList, {
+            status: StudentStatus.available,
+            scheduledAt: null,
+            hr: null,
+        });
+    }
+}
