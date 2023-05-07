c1df9dc28c9c41a2345b743f31f44be38960b80a
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-05-07 13:02:34 +0200

    Added modules imports and mailing to hireStudent method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
-dotenv.config();
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { ormConfig } from '../ormconfig';
 import { UserModule } from './user/user.module';
+import { EmailModule } from './email/email.module';
+
+dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), StudentModule, UserModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), StudentModule, UserModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
