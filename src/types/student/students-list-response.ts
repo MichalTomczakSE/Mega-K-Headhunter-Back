f18e9fcfa8cd31c5e7c06f8ab6998404ad63ac33
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-05-10 16:10:57 +0200

    Added data example.

--- src/types/student/students-list-response.ts
+++ src/types/student/students-list-response.ts
@@ -1,4 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class StudentListItem {
+
     id: string;
     firstName: string;
     lastName: string;
@@ -7,6 +10,22 @@ export class StudentListItem {
 }
 
 export class StudentsListResponse {
+  @ApiProperty({
+    description: 'Students list',
+    type: [StudentListItem],
+    example: [
+      {
+        id: 2,
+        firstName: 'Jan',
+        lastName: 'Kowalski',
+        githubUsername: 'Kowal',
+        scheduledAt: new Date(),
+      },
+  })
     students: StudentListItem[];
+    @ApiProperty({
+        description: 'Total',
+        example: '22',
+    })
     totalPages: number;
 }
