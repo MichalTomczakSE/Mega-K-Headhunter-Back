7eed9579ada06876c5a0e3e38dadefc38ff5ec9e
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-05-10 16:05:54 +0200

    Added ApiProperty example.

--- src/types/student/update-student-details-response.ts
+++ src/types/student/update-student-details-response.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateStudentDetailsResponse {
+  @ApiProperty({ description: 'Student status', example: 'changed'})
   status: string;
 }
