2e3a96dc329097911d32cd07196ffb43cbd140dd
ZrodoZrodo <112622013+ZrodoZrodo@users.noreply.github.com>
2023-05-13 20:01:16 +0200

    auth fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Request, UseGuards } from "@nestjs/common";
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
+
+interface loginUser {
+  token: string;
+  id: string;
+  role: number;
+  email: string;
+}
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @HttpCode(200)
   @Post('/login')
-  async login(@Request() req) {
+  async login(@Request() req): Promise<loginUser> {
     return {
       token: await this.authService.login(req.user),
       ...req.user,
